222b643d907420e7f11029bd69a2ad4785b05b74
Aozhen <aozhen01@gmail.com>
2020-11-17 19:18:39 -0600

    fixed parsing exceptions

--- XML/Painter.hpp
+++ XML/Painter.hpp
@@ -3,29 +3,34 @@
 #include "../Model/Color.hpp"
 #include "XMLParser.hpp"
 
-class Painter{
+class Painter
+{
 
 private:
-    XMLParser vista;
+    XMLParser *vista;
 
 public:
-    Painter(){}
-
-    void pintarPais(vector<Color*> pColores, string pRuta){
-        //codigo pintar
-        int cantidadPintados = 0;
-        for(Color * color : pColores){
-            cantidadPintados += color->getPaises().size();
-            for(Country pais: color->getPaises()){
-                vista.cambiarColorPais(pais.getCountryName(), color->getColorCode());
-            }
-        }
-        vista.cambiarCantidadPaisesEnBlanco(211 - cantidadPintados);
-        vista.cambiarCantidadPaisesPintados(cantidadPintados);
-        vista.escribirASVG(pRuta);
+    Painter(XMLParser *parser)
+    {
+        vista = parser;
     }
 
-
+    void pintarPais(vector<Color *> pColores, string pRuta)
+    {
+        //codigo pintar
+        int cantidadPintados = 0;
+        for (Color *color : pColores)
+        {
+            cantidadPintados += color->getPaises().size();
+            for (Country pais : color->getPaises())
+            {
+                vista->cambiarColorPais(pais.getCountryName(), color->getColorCode());
+            }
+        }
+        vista->cambiarCantidadPaisesEnBlanco(211 - cantidadPintados);
+        vista->cambiarCantidadPaisesPintados(cantidadPintados);
+        vista->escribirASVG(pRuta);
+    }
 };
 
 #endif
